2f18109b5e4cfc24df3ff752d1bc94a63546c255
t.i <isoittech@gmail.com>
2020-09-25 15:13:01 +0900

    問題修正：テーブルIDの暗号化後文字列がBASE64になっていなかった

--- backend/src/helper/endecode.ts
+++ backend/src/helper/endecode.ts
@@ -5,45 +5,78 @@ const ENCRYPTION_KEY = 'CRYPT_SEED_FOR_ID';
 const SALT = 'KayNQW7vHC8C+CP8NwxCfw==';
 const IV = crypto.randomBytes(16);
 
+
 export const createScheduleUpdateId = (scheduleUpdateIdSeed: number): string => {
     try {
+        const cipher = crypto.createCipher(ALGORITHM, ENCRYPTION_KEY)
+        let crypted = cipher.update('<crypt>' + String(scheduleUpdateIdSeed) + '</crypt>', 'utf8', 'base64')
+        crypted += cipher.final('base64');
 
-        // 鍵を生成
-        const key = crypto.scryptSync(ENCRYPTION_KEY, SALT, 32)
-        // 暗号器を生成
-        const cipher = crypto.createCipheriv(ALGORITHM, key, IV)
-        // 暗号化したいメッセージ文字列を Buffer に変換
-        const data = Buffer.from('<crypt>' + String(scheduleUpdateIdSeed) + '</crypt>')
-        // data を暗号化
-        let encryptedData = cipher.update(data)
-        encryptedData = Buffer.concat([encryptedData, cipher.final()])
+        return crypted;
 
-        return encryptedData
     } catch (error) {
         console.log(`[ERROR] create_schedule_update_idでエラー。引数:${scheduleUpdateIdSeed}`);
         throw error;
     }
 }
 
-
-export const decodeFromScheduleUpdateId = (scheduleUpdateId: string): number => {
+const decodeFromScheduleUpdateId = (scheduleUpdateId: string): number => {
     try {
+        const decipher = crypto.createDecipher(ALGORITHM, ENCRYPTION_KEY)
+        let dec = decipher.update(scheduleUpdateId, 'base64', 'utf8')
+        dec += decipher.final('utf8');
+        dec = dec.match(/<crypt>(.*)<\/crypt>/);
+        dec = dec[1];
 
-        // 鍵を生成
-        const key = crypto.scryptSync(ENCRYPTION_KEY, SALT, 32)
+        return dec;
 
-        // 復号器を生成
-        const decipher = crypto.createDecipheriv(ALGORITHM, key, IV)
-
-        // encryptedData を復号
-        let decryptedData: any = decipher.update(scheduleUpdateId)
-        decryptedData = Buffer.concat([decryptedData, decipher.final()]);
-        decryptedData = decryptedData.match(/<crypt>(.*)<\/crypt>/);
-        decryptedData = decryptedData[1];
-
-        return decryptedData
     } catch (error) {
         console.log(`[ERROR] decode_from_schedule_update_idでエラー。引数:${scheduleUpdateId}`);
         throw error;
     }
+
 }
+
+//
+// export const createScheduleUpdateId = (scheduleUpdateIdSeed: number): string => {
+//     try {
+//
+//         // 鍵を生成
+//         const key = crypto.scryptSync(ENCRYPTION_KEY, SALT, 32)
+//         // 暗号器を生成
+//         const cipher = crypto.createCipheriv(ALGORITHM, key, IV)
+//         // 暗号化したいメッセージ文字列を Buffer に変換
+//         const data = Buffer.from('<crypt>' + String(scheduleUpdateIdSeed) + '</crypt>')
+//         // data を暗号化
+//         let encryptedData = cipher.update(data)
+//         encryptedData = Buffer.concat([encryptedData, cipher.final()])
+//
+//         return encryptedData
+//     } catch (error) {
+//         console.log(`[ERROR] create_schedule_update_idでエラー。引数:${scheduleUpdateIdSeed}`);
+//         throw error;
+//     }
+// }
+//
+//
+// export const decodeFromScheduleUpdateId = (scheduleUpdateId: string): number => {
+//     try {
+//
+//         // 鍵を生成
+//         const key = crypto.scryptSync(ENCRYPTION_KEY, SALT, 32)
+//
+//         // 復号器を生成
+//         const decipher = crypto.createDecipheriv(ALGORITHM, key, IV)
+//
+//         // encryptedData を復号
+//         let decryptedData: any = decipher.update(scheduleUpdateId)
+//         decryptedData = Buffer.concat([decryptedData, decipher.final()]);
+//         decryptedData = decryptedData.match(/<crypt>(.*)<\/crypt>/);
+//         decryptedData = decryptedData[1];
+//
+//         return decryptedData
+//     } catch (error) {
+//         console.log(`[ERROR] decode_from_schedule_update_idでエラー。引数:${scheduleUpdateId}`);
+//         throw error;
+//     }
+// }
