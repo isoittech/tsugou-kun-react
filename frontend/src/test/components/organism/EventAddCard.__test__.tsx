2604d03e28defb91d5bdb301607a24717d3f5135
t.i <isoittech@gmail.com>
2020-11-01 12:04:40 +0900

    いったんテストケースから排除

---
+++ frontend/src/test/components/organism/EventAddCard.__test__.tsx
@@ -0,0 +1,10 @@
+// import React, { useState, useEffect } from "react";
+// import { render, screen, cleanup } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+// import { EventAddCard } from "../../../containers/organism/EventAddCard";
+
+// describe("レンダリング確認", () => {
+//     it("前提条件なしで主要タグ・属性値が正しくレンダリングされていること", () => {
+//         // render(<EventAddCard />)
+//     });
+// });
