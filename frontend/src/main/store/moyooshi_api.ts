d592a230f5fdf4400af7636bdf53ae6aac234ed8
t.i <isoittech@gmail.com>
2021-01-17 13:30:38 +0900

    コメント誤り

--- frontend/src/main/store/moyooshi_api.ts
+++ frontend/src/main/store/moyooshi_api.ts
@@ -11,11 +11,13 @@ export const ApiExecutionStateType = {
     UPDATE_FAILED: "UPDATE_FAILED",
 } as const;
 // 「as const」というConstアサーションを利用。
-// これを利用することで、後続の宣言EventAction.typeの型が「string」ではなくリテラル型の「ADD_EVENT|SUCCESS_～|FAIL_～…」となる。
+// これを利用することで、後続の宣言moyooshiAddApiStatusの型が
+// 「string」ではなくリテラル型の「ADD_EVENT|SUCCESS_～|FAIL_～…」となる。
 
 // ValueOf: 「keyof 型」という文で、オブジェクトの型からキーを取り出してくれる自前の型。
 export type ApiExecutionState = {
-    // 下記は、EventAction.typeの型を、EventActionTypeの中で定義したリテラル型の「ADD_EVENT|SUCCESS_～|FAIL_～…」にしてくれる。
+    // 下記は、moyooshiAddApiStatusの型を、
+    // ApiExecutionStateTypeの中で定義したリテラル型の「ADD_EVENT|SUCCESS_～|FAIL_～…」にしてくれる。
     // ※前述「as const」と組み合わせる必要あり。
     moyooshiAddApiStatus: ValueOf<typeof ApiExecutionStateType>;
     // APIからの返却オブジェクト
