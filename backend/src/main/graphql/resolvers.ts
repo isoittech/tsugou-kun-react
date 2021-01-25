8492859e667423be9b46afab05b128390a2d2c51
t.i <isoittech@gmail.com>
2021-01-26 00:34:43 +0900

    不要なため。

--- backend/src/main/graphql/resolvers.ts
+++
@@ -1,9 +0,0 @@
-import * as moyooshi_service from "../service/moyooshi_service";
-import { MoyooshiServiceOutputDto } from "../service/moyooshi_service";
-import * as endecode from "../helper/endecode";
-
-export const getMoyooshi = async (schedule_update_id: number) => {
-    // const moyooshiId: number = endecode.decodeFromScheduleUpdateId(schedule_update_id);
-    const result: MoyooshiServiceOutputDto = await moyooshi_service.readMoyooshi(schedule_update_id);
-    return result;
-};
